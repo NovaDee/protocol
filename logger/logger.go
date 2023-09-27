8265e6d189a133d01288bfce06e29346df9d2903
张峰2359 <zf2359@hanweb.com>
2023-09-27 11:48:34 +0800

    fix：代码修改

--- logger/logger.go
+++ logger/logger.go
@@ -212,6 +212,117 @@ func (l *ZapLogger) ToZap() *zap.SugaredLogger {
 	return l.zap
 }
 
+type LogRLogger logr.Logger
+
+func (l *ZapLogger) isEnabled(level zapcore.Level) bool {
+	return level >= l.level.Level()
+}
+
+func (l *ZapLogger) Debugw(msg string, keysAndValues ...interface{}) {
+	if !l.isEnabled(zapcore.DebugLevel) {
+		return
+	}
+	l.zap.Debugw(msg, keysAndValues...)
+}
+
+func (l *ZapLogger) Infow(msg string, keysAndValues ...interface{}) {
+	if !l.isEnabled(zapcore.InfoLevel) {
+		return
+	}
+	l.zap.Infow(msg, keysAndValues...)
+}
+
+func (l *ZapLogger) Warnw(msg string, err error, keysAndValues ...interface{}) {
+	if !l.isEnabled(zapcore.WarnLevel) {
+		return
+	}
+	if err != nil {
+		keysAndValues = append(keysAndValues, "error", err)
+	}
+	l.zap.Warnw(msg, keysAndValues...)
+}
+
+func (l *ZapLogger) Errorw(msg string, err error, keysAndValues ...interface{}) {
+	if !l.isEnabled(zapcore.ErrorLevel) {
+		return
+	}
+	if err != nil {
+		keysAndValues = append(keysAndValues, "error", err)
+	}
+	l.zap.Errorw(msg, keysAndValues...)
+}
+
+func (l *ZapLogger) WithValues(keysAndValues ...interface{}) Logger {
+	dup := *l
+	dup.zap = l.zap.With(keysAndValues...)
+	// mirror unsampled logger too
+	if l.unsampled == l.zap {
+		dup.unsampled = dup.zap
+	} else {
+		dup.unsampled = l.unsampled.With(keysAndValues...)
+	}
+	return &dup
+}
+
+func (l *ZapLogger) WithName(name string) Logger {
+	dup := *l
+	dup.zap = l.zap.Named(name)
+	if l.unsampled == l.zap {
+		dup.unsampled = dup.zap
+	} else {
+		dup.unsampled = l.unsampled.Named(name)
+	}
+	return &dup
+}
+
+func (l *ZapLogger) WithComponent(component string) Logger {
+	// zap automatically appends .<name> to the logger name
+	dup := l.WithName(component).(*ZapLogger)
+	if dup.component == "" {
+		dup.component = component
+	} else {
+		dup.component = dup.component + "." + component
+	}
+	dup.level = dup.sharedConfig.setEffectiveLevel(dup.component)
+	return dup
+}
+
+func (l *ZapLogger) WithCallDepth(depth int) Logger {
+	dup := *l
+	dup.zap = l.zap.WithOptions(zap.AddCallerSkip(depth))
+	if l.unsampled == l.zap {
+		dup.unsampled = dup.zap
+	} else {
+		dup.unsampled = l.unsampled.WithOptions(zap.AddCallerSkip(depth))
+	}
+	return &dup
+}
+
+func (l *ZapLogger) WithItemSampler() Logger {
+	if l.SampleDuration == 0 {
+		return l
+	}
+	dup := *l
+	dup.zap = l.unsampled.WithOptions(zap.WrapCore(func(core zapcore.Core) zapcore.Core {
+		return zapcore.NewSamplerWithOptions(
+			core,
+			l.SampleDuration,
+			l.SampleInitial,
+			l.SampleInterval,
+		)
+	}))
+	return &dup
+}
+
+func (l *ZapLogger) WithoutSampler() Logger {
+	if l.unsampled == l.zap {
+		return l
+	}
+	dup := *l
+	dup.zap = l.unsampled
+	return &dup
+}
+
 // 动态更新日志等级，后续使用
 func (cfg *sharedConfig) onConfigUpdate(conf *Config) error {
 	// 设置最新的日志等级
